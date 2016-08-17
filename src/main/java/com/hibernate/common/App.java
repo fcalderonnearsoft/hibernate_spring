70553c72ff05a27b6067fb140e7f7297ee1c0073
fcalderon <fcalderon@switchfly.com>
2016-08-17 11:39:46 -0600

    create test to check if data are commited correctly

--- src/main/java/com/hibernate/common/App.java
+++ src/main/java/com/hibernate/common/App.java
@@ -7,57 +7,31 @@ import java.util.Date;
 
 public class App {
 
-    public static void main( String[] args )
-    {
-       // createSampleData();
+    public static void main(String[] args) {
     }
 
 
-
-    public void createSampleData(){
+    public void createSampleData(int goal, int total, String stockCode, String stockName) {
         Session session = HibernateUtil.getSessionFactory().openSession();
 
         session.beginTransaction();
 
         Stock stockOne = new Stock();
 
-        stockOne.getProteinData().setGoal(3000);
-        stockOne.addStockHistoryList(new StockHistory(new Date(), "Set goal code to 3000"));
-        stockOne.getProteinData().setTotal(250);
-        stockOne.addStockHistoryList(new StockHistory(new Date(), "Set total code to 250"));
-        stockOne.setStockCode("EWRGC");
-        stockOne.addStockHistoryList(new StockHistory(new Date(), "Set stock code to EWRGC"));
-        stockOne.setStockName("First of stock one");
-        stockOne.addStockHistoryList(new StockHistory(new Date(), "Set stock name to First"));
-
-        Stock stockTwo = new Stock();
-
-        stockTwo.getProteinData().setGoal(10000);
-        stockTwo.addStockHistoryList(new StockHistory(new Date(), "Set goal code to 10000"));
-        stockTwo.getProteinData().setTotal(7500);
-        stockTwo.addStockHistoryList(new StockHistory(new Date(), "Set total code to 7500"));
-        stockTwo.setStockCode("PRTK");
-        stockTwo.addStockHistoryList(new StockHistory(new Date(), "Set stock code to PRTK"));
-        stockTwo.setStockName("First of stock two");
-        stockTwo.addStockHistoryList(new StockHistory(new Date(), "Set stock name to First"));
-
-        Stock stockThree = new Stock();
-
-        stockThree.getProteinData().setGoal(7600);
-        stockThree.addStockHistoryList(new StockHistory(new Date(), "Set goal code to 760"));
-        stockThree.getProteinData().setTotal(10);
-        stockThree.addStockHistoryList(new StockHistory(new Date(), "Set total code to 10"));
-        stockThree.setStockCode("QTRXCD");
-        stockThree.addStockHistoryList(new StockHistory(new Date(), "Set stock code to QTRXCD"));
-        stockThree.setStockName("First of stock three");
-        stockThree.addStockHistoryList(new StockHistory(new Date(), "Set stock name to First"));
+        stockOne.getProteinData().setGoal(goal);
+        stockOne.addStockHistoryList(new StockHistory(new Date(), "Set goal code to " + goal));
+        stockOne.getProteinData().setTotal(total);
+        stockOne.addStockHistoryList(new StockHistory(new Date(), "Set total code to " + total ));
+        stockOne.setStockCode(stockCode);
+        stockOne.addStockHistoryList(new StockHistory(new Date(), "Set stock code to " + stockCode));
+        stockOne.setStockName(stockName);
+        stockOne.addStockHistoryList(new StockHistory(new Date(), "Set stock name to " +stockName));
 
         session.save(stockOne);
-        session.save(stockTwo);
-        session.save(stockThree);
-        session.refresh(stockOne);
-        session.refresh(stockTwo);
-        session.refresh(stockThree);
-        session.flush();
+//        session.refresh(stockOne);
+//        session.refresh(stockTwo);
+//        session.refresh(stockThree);
+        session.getTransaction().commit();
+        session.close();
     }
 }
--- src/test/java/com/hibernate/common/AppTest.java
+++ src/test/java/com/hibernate/common/AppTest.java
@@ -15,19 +15,35 @@ public class AppTest {
     public void testCreateSampleData() {
         App app = new App();
 
-        app.createSampleData();
+        int goal = 3000, total = 250;
+        String stockCode = "ERCFEW", stockName = "First of wednesday";
+
+        app.createSampleData(goal, total, stockCode, stockName);
 
         Session session = HibernateUtil.getSessionFactory().openSession();
-        //session.beginTransaction();
+
+
         List<Stock> stockList = session.createQuery("from Stock").list();
 
-        for(Stock stock : stockList){
-            System.out.println(stock.getStockName());
-        }
+        Stock stock = stockList.get(0);
+        assertEquals(stock.getStockCode(), stockCode);
+        assertEquals(stock.getStockName(), stockName);
+
+//        session.beginTransaction();
+//        Query deleteQuery = session.createQuery("delete from ProteinData where id = :id");
+//        deleteQuery.setParameter("id", stock.getStockId());
+//        deleteQuery.executeUpdate();
+//
+//        deleteQuery = session.createQuery("delete from StockHistory where id = :id");
+//        deleteQuery.setParameter("id", stock.getStockId());
+//        deleteQuery.executeUpdate();
+
+//        Query deleteQuery = session.createQuery("delete from Stock where stockId = :id");
+//        deleteQuery.setParameter("id", stock.getStockId());
+//        deleteQuery.executeUpdate();
 
 
-
-        session.getTransaction().rollback();
+//        session.getTransaction().commit();
         session.close();
     }
 }
